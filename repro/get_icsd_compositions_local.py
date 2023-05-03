564f60d35844201d972b0b7566972102d409f6b9
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-05-03 11:04:24 -0700

    add repro/get_icsd_compositions_mp.py and repro/avail-icsd-from-mp.csv
    move all repro related files to new repro/ dir

--- repro/get_icsd_compositions_local.py
+++ repro/get_icsd_compositions_local.py
@@ -4,7 +4,6 @@ import warnings
 
 import pandas as pd
 from pymatgen.core import Structure
-from pymatviz import count_elements, ptable_heatmap_plotly
 from tqdm import tqdm
 
 __author__ = "Janosh Riebesell"
@@ -13,13 +12,14 @@ __date__ = "2023-05-02"
 ICSD_PATH = "/Users/janosh/dev/icsd/icsd_cifs"
 
 # %%
-with open("icsd_full_data_unique_no_frac_no_penta_2020_icsd_codes_only.txt", "r") as f:
-    synthnn_train_ids = f.read().splitlines()
+train_ids_path = "icsd_full_data_unique_no_frac_no_penta_2020_icsd_codes_only.txt"
+with open(train_ids_path) as file:
+    synthnn_train_ids = list(map(int, file.read().splitlines()))
 
 
 # %%
 avail_icsd_ids = [
-    filename.replace(".cif", "").replace("icsd_", "")
+    int(filename.replace(".cif", "").replace("icsd_", ""))
     for filename in os.listdir(ICSD_PATH)
 ]
 
@@ -34,6 +34,7 @@ assert len({*avail_icsd_ids}) == len(
 
 # %%
 intersect = {*synthnn_train_ids} & {*avail_icsd_ids}
+missing = [*{*synthnn_train_ids} - {*avail_icsd_ids}]
 
 print(f"{len(avail_icsd_ids)=:,}")
 print(f"{len(synthnn_train_ids)=:,}")
@@ -66,23 +67,16 @@ for icsd_id in tqdm(intersect):
         structures[icsd_id] = struct
 
 
-# %% get composition for each structure
+# %% write IDs with their composition back to disk as CSV
 compositions = {
     icsd_id: struct.composition.alphabetical_formula
     for icsd_id, struct in structures.items()
 }
-
-elem_counts = count_elements(list(compositions.values()), count_mode="occurrence")
-
-
-# %% plot elemental occurrence heatmap
-fig = ptable_heatmap_plotly(elem_counts[elem_counts > 1], log=True)
-fig.layout.title = "Elemental occurrence of available SynthNN ICSD training data"
-fig.show()
-
-
-# %% write IDs with their composition back to disk as CSV
 id_col = "icsd_id"
-df_avail_formulas = pd.DataFrame(compositions.items(), columns=[id_col, "composition"])
+df_avail_formulas = pd.DataFrame(
+    compositions.items(), columns=[id_col, "composition"]
+).set_index(id_col)
+df_avail_formulas["n_atoms"] = [len(struct) for struct in structures.values()]
+df_avail_formulas["n_elements"] = [len(comp) for comp in compositions.values()]
 
-df_avail_formulas.set_index(id_col).to_csv("avail-icsd-compositions.csv")
+df_avail_formulas.to_csv("avail-icsd-from-local.csv")
